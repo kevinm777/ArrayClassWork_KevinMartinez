71194edb063bce87e1309029df0e4aefa7503634
Kevin Martinez Palacios <69471354+kevinm777@users.noreply.github.com>
2021-01-29 11:55:02 -0600

    Add files via upload

---
+++ Array_ClassWork.js
@@ -0,0 +1,7 @@
+var movies = ["Game of Thrones","Star Wars","Superman","Spiderman","Avengers","End Game"];
+movies.add("Titanic");
+movies.unshift("Avatar");
+movies.pop();
+movies.shift();
+
+console.log(movies);
